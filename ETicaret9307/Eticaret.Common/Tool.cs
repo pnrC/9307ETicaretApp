1ab1ebd881fdbef992bdaee04b51692ead7012ae
pinar.can <pinar.can@ALS-YMS-H05>
2019-07-09 21:12:56 +0300

    Entity model eklendi. DB bağlantısı yapıldı.

---
+++ ETicaret9307/Eticaret.Common/Tool.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaret.Entity; //EntityCommona referans sag tık add entity yi refere ettik
+
+namespace Eticaret.Common
+{
+    public class Tool
+    {
+        public static ECommerceEntities db = null;
+
+        public static ECommerceEntities GetConnection()
+        {
+            if(db is null)
+            {
+                db = new ECommerceEntities();
+            }
+            return db;
+        }       
+    }
+}
