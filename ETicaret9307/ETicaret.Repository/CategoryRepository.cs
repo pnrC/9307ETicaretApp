cc84777af4e590d8cedd9d76357b4378a800fe43
pinar.can <pinar.can@ALS-YMS-H05>
2019-07-10 21:05:51 +0300

    Admin template'i eklendi.

---
+++ ETicaret9307/ETicaret.Repository/CategoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eticaret.Common; //biz ekledik
+using ETicaret.Entity; //biz ekledik
+
+namespace ETicaret.Repository
+{
+    class CategoryRepository : DataRepository<Category, Guid>
+    {
+        static ECommerceEntities db = Tool.GetConnection();
+        ResultProcess<Category> result = new ResultProcess<Category>();
+
+        public override Result<int> Delete(Guid id)
+        {
+            Category c = db.Categories.SingleOrDefault(x => x.CategoryID == id);
+            db.Categories.Remove(c);
+            return result.GetResult(db);
+        }
+
+        public override Result<Category> GetObjByID(Guid ID)
+        {
+            Category c = db.Categories.SingleOrDefault(x => x.CategoryID == ID);
+            return result.GetT(c);
+        }
+
+        public override Result<int> Insert(Category item)
+        {
+            Category yeni = db.Categories.Create();
+            yeni.CategoryName = item.CategoryName;
+            yeni.CreatedDate = DateTime.Now;
+            yeni.Description = item.Description;
+            //burda sadece saf property'lerini işliyorum.
+            db.Categories.Add(yeni);
+
+            return result.GetResult(db); //çunku sadece GetResult içinde db.savechanges var ve benim lu an db.savechanges yapmam alzım.
+        }
+
+        public override Result<List<Category>> List()
+        {
+            List<Category> CatList = db.Categories.ToList();
+            return result.GetListResult(CatList);
+        }
+
+        public override Result<int> Update(Category item)
+        {
+            Category dbdeki = db.Categories.SingleOrDefault(x => x.CategoryID == item.CategoryID);
+            dbdeki.CategoryName = item.CategoryName;
+            dbdeki.Description = item.Description;
+            //dbdeki.CreatedDate= bunu da guncelleyemeyiz updateDate diye bir şey yaratmadım,ilk yarattıgımda date i de yaratıldı zaten
+            return result.GetResult(db);
+        }
+    }
+}
---
+++ ETicaret9307/ETicaret.Repository/DataRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eticaret.Common; //Eticaret.Repository> referance> Add referance > common'ı ekledik
+
+namespace ETicaret.Repository
+{
+    public abstract class DataRepository<T, M>
+    {
+        //Burda temel CRUD işlemerini temsil edecek metotlarım var
+        public abstract Result<int> Insert(T item);
+
+        public abstract Result<int> Update(T item);
+
+        public abstract Result<int> Delete(M id);
+
+        public abstract Result<List<T>> List();
+
+        public abstract Result<T> GetObjByID(M ID);
+    }
+}
