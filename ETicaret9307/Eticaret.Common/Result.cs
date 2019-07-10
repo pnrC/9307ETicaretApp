35e639b2f0955d50092376c46ba3a301436cda41
pinar.can <pinar.can@ALS-YMS-H05>
2019-07-10 19:48:43 +0300

    Common'daki tüm class lar oluşturuldu.

---
+++ ETicaret9307/Eticaret.Common/Result.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eticaret.Common
+{
+    public class Result<T> //Bu class Generic Type'ta yani bu Tye ne koyarsam o gelcek
+    {
+        //Helper Class: Belirli task lar altında nalamlı hale gelen class lar , helper class da denir.
+        public string UserMessage { get; set; }
+        public bool IsSucceeded { get; set; }
+        public T ProcessResult { get; set; } //Bu T yi kullanırken istediğimi atarım. Örnegin Category çekiyorum o zaman T yerine Category yerleşecek.
+    }
+}
---
+++ ETicaret9307/Eticaret.Common/ResultProcess.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ETicaret.Entity;
+
+namespace Eticaret.Common
+{
+    public class ResultProcess<T> //Generic class: içinde T tag'i var ve kullanırken ne atarsam o oalcak.
+    {
+        public Result<int> GetResult(ECommerceEntities db) //Bu metot: db'ye kayıt yapmak için kullanacağımız yardımcı metot.
+        {
+            Result<int> result = new Result<int>();
+
+            int sonuc = db.SaveChanges();
+
+            if (sonuc > 0)
+            {
+                result.UserMessage = "Basarili";
+                result.IsSucceeded = true;
+                result.ProcessResult = sonuc;
+            }
+            else
+            {
+                result.UserMessage = "Basarisiz";
+                result.IsSucceeded = false;
+                result.ProcessResult = sonuc;
+            }
+
+            return result;
+        }
+
+        public Result<List<T>> GetListResult(List<T> data) //Bu metot: db'den Liste tipinde data çekerken, datanın olup olmadıgını kontrol eden metot
+        {
+            Result<List<T>> result = new Result<List<T>>();
+
+            if(data != null)
+            {
+                result.UserMessage = "Işlem başarılı";
+                result.IsSucceeded = true;
+                result.ProcessResult = data;
+            }
+            else
+            {
+                result.UserMessage = "Işlem başarısız data yok";
+                result.IsSucceeded = true;
+                result.ProcessResult = data;
+            }
+            return result;
+        }
+
+        public Result<T> GetT(T data) //Bu metot: db'den tek bir item çekmek için- tek bir özellik
+        {
+            Result<T> result = new Result<T>();
+
+            if(data != null)
+            {
+                result.IsSucceeded = true;
+                result.UserMessage = "Basarili";
+                result.ProcessResult = data;
+            }
+            else
+            {
+                result.IsSucceeded = false;
+                result.UserMessage = "Basarisiz";
+                result.ProcessResult = data;
+            }
+            return result;
+        }
+    }
+}
